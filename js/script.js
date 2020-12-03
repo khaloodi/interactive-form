f3a21c40528fbbe43df2ac0afefaf358cbefbc1e
Khaled Adad <khaledadad@lewisu.edu>
2020-12-02 21:51:54 -0600

    Make other under job category disappear

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 const name = document.getElementById("name")
 name.focus()
+
+const jobRole = document.getElementById("title")
+const otherRole = document.getElementById("other-job-role")
+
+otherRole.style.display = "none"
+jobRole.addEventListener("change", e => {
+    console.log(e.target.value)
+    if (e.target.value === "other") {
+        otherRole.style.display = "inline-block"
+    } else {
+        otherRole.style.display = "none"
+    }
+})
