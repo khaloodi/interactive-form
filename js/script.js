6803bf483a2ba7e358d25941779f74b0d1c88c14
Khaled Adad <khaledadad@lewisu.edu>
2020-12-08 10:08:05 -0600

    WIP

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
+// make it so that on page hole the form focuses on name input field
 const name = document.getElementById("name")
 name.focus()
 
+// make it so that the other dropdown disappears unless clicked
 const jobRole = document.getElementById("title")
 const otherRole = document.getElementById("other-job-role")
-
 otherRole.style.display = "none"
+
 jobRole.addEventListener("change", e => {
     if (e.target.value === "other") {
         otherRole.style.display = "inline-block"
     } else {
         otherRole.style.display = "none"
+    }
+})
+
+// only color options for selected design
+const design = document.getElementById("design")
+const color = document.getElementById("shirt-colors")
+color.style.display = "none"
+
+const colorOptions = document.querySelectorAll('[data-theme]')
+
+design.addEventListener("change", e => {
+    if (e.target.value === "js puns") {
+        color.style.display = "inline-block"
+        return null
+    } else if (e.target.value === "heart js") {
+        color.style.display = "inline-block"
+        return null
     }
 })
