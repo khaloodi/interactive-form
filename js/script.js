b954121d73e52178f2c85e22e49af7fb18d477c0
Khaled Adad <khaledadad@lewisu.edu>
2021-03-08 12:00:35 -0600

    wip

--- js/script.js
+++ js/script.js
@@ -116,15 +116,22 @@ function activitiesValidator() {
 
 function paymentValidator() {
     const cardNumber = document.getElementById('cc-num')
-    const cardNumberIsValid = /^(?:(4[0-9]{12}(?:[0-9]{3})?)|(5[1-5][0-9]{14})|(6(?:011|5[0-9]{2})[0-9]{12})|(3[47][0-9]{13})|(3(?:0[0-5]|[68][0-9])[0-9]{11})|((?:2131|1800|35[0-9]{3})[0-9]{11}))$/.test(cardNumber)
-    payment.value === 'credit-card' ? true : false
+    const cardNumberIsValid = /^[0-9]{13,16}$/.test(cardNumber.value)
 
     let paymentIsValid;
-    if (cardNumberIsValid && payment.value) {
+    if (payment.value === 'credit-card') {
+        if (cardNumberIsValid) {
+            paymentIsValid = true;
+        }
+    } else if (payment.value === 'paypal') {
+        paymentIsValid = true;
+    } else if (payment.value === 'bitcoin') {
         paymentIsValid = true;
     } else {
         paymentIsValid = false;
     }
+
+
 
     console.log((`Payment section validation test evaluates to ${paymentIsValid}`))
     return paymentIsValid
@@ -133,7 +140,7 @@ function paymentValidator() {
 const zipcode = document.getElementById('zip')
 
 function zipcodeValidator() {
-    const zipcodeIsValid = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zipcode.value)
+    const zipcodeIsValid = /^[0-9]{5}$/.test(zipcode.value)
     console.log((`Zipcode value is: ${zipcode.value}`))
     return zipcodeIsValid
 }
@@ -186,11 +193,9 @@ form.addEventListener('submit', e => {
     }
 
     if (!paymentValidator()) {
-        console.log('Activities validator prevented submission')
+        console.log('Payment validator prevented submission')
         creditCard.parentElement.classList.add("not-valid")
         creditCard.parentElement.classList.remove("valid")
-            // the line below was causing the bitcoin option to be displayed on form submission
-            // creditCard.parentElement.lastElementChild.style.display = ""
         e.preventDefault()
     } else if (paymentValidator()) {
         creditCard.parentElement.classList.add("valid")
@@ -198,29 +203,33 @@ form.addEventListener('submit', e => {
         creditCard.parentElement.lastElementChild.style.display = "none"
     }
 
-    if (!zipcodeValidator()) {
-        console.log('Zipcode validator prevented submission')
-        zipcode.parentElement.classList.add("not-valid")
-        zipcode.parentElement.classList.remove("valid")
-        zipcode.parentElement.lastElementChild.style.display = ""
-        e.preventDefault()
-    } else if (zipcodeValidator()) {
-        zipcode.parentElement.classList.add("valid")
-        zipcode.parentElement.classList.remove("not-valid")
-        zipcode.parentElement.lastElementChild.style.display = "none"
+    if (payment.value === 'credit-card') {
+
+        if (!zipcodeValidator()) {
+            console.log('Zipcode validator prevented submission')
+            zipcode.parentElement.classList.add("not-valid")
+            zipcode.parentElement.classList.remove("valid")
+            zipcode.parentElement.lastElementChild.style.display = ""
+            e.preventDefault()
+        } else if (zipcodeValidator()) {
+            zipcode.parentElement.classList.add("valid")
+            zipcode.parentElement.classList.remove("not-valid")
+            zipcode.parentElement.lastElementChild.style.display = "none"
+        }
+
+        if (!cvvValidator()) {
+            console.log('CVV validator prevented submission')
+            cvv.parentElement.classList.add("not-valid")
+            cvv.parentElement.classList.remove("valid")
+            cvv.parentElement.lastElementChild.style.display = ""
+            e.preventDefault()
+        } else if (cvvValidator()) {
+            cvv.parentElement.classList.add("valid")
+            cvv.parentElement.classList.remove("not-valid")
+            cvv.parentElement.lastElementChild.style.display = "none"
+        }
     }
 
-    if (!cvvValidator()) {
-        console.log('CVV validator prevented submission')
-        cvv.parentElement.classList.add("not-valid")
-        cvv.parentElement.classList.remove("valid")
-        cvv.parentElement.lastElementChild.style.display = ""
-        e.preventDefault()
-    } else if (cvvValidator()) {
-        cvv.parentElement.classList.add("valid")
-        cvv.parentElement.classList.remove("not-valid")
-        cvv.parentElement.lastElementChild.style.display = "none"
-    }
 
     // e.preventDefault()
 })
